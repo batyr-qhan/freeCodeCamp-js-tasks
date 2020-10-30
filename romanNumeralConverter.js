4a5be22e15401aa78f62f7635950f4e01cb05529
Batyrkhan <batyrqh@protonmail.com>
2020-10-30 23:16:14 +0600

    roman numbers converter done. It's terrible solution but it worked.

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -103,3 +103,21 @@ function convertToRoman (num) {
 }
 
 convertToRoman(3996)
+
+// and beautiful solution
+
+function solution(number)
+{
+  var result   = '',
+    decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+    roman    = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  decimals.map(function (value, index) {
+    while (number >= value) {
+      result += roman[index];
+      number -= value;
+    }
+  });
+
+  return result;
+}
